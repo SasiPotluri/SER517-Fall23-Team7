212b1ee4a11cb6a7396a12adb94cb5a5f316930b
SasiPotluri <98196566+SasiPotluri@users.noreply.github.com>
2023-09-15 17:40:03 -0700

    modified DETR algorithm by placing placeholders for future implementation

--- training_DETr_AOD.py
+++ training_DETr_AOD.py
@@ -7,56 +7,80 @@ from torchvision.datasets import CocoDetection
 from torchvision import transforms
 from torch.utils.data import DataLoader
 
+
+# Define your custom dataset
 class AerialRescue(torch.utils.data.Dataset):
     def __init__(self, data_dir, transforms=None):
         # Initialize your dataset here, load images, and annotations
-        pass
+        # You may need to implement a function to parse annotations and load images
+        self.data_dir = data_dir
+        self.transforms = transforms
+        # Load your data and annotations here
+        # Example: self.images = ...  # List of image file paths
+        # Example: self.annotations = ...  # List of annotation dictionaries
 
     def __len__(self):
         return len(self.images)
 
     def __getitem__(self, idx):
         # Implement this method to return an image and its annotations
+        image_path = self.images[idx]
+        # Load and preprocess the image
+        image = self.load_image(image_path)
+        if self.transforms:
+            image = self.transforms(image)
+
+        # Load and preprocess annotations (e.g., bounding boxes)
+        # Modify this part according to your dataset's annotation format
+        annotation = self.load_annotations(idx)
+        return image, annotation
+
+    def load_image(self, image_path):
+        # Implement this method to load and preprocess images
         pass
+
+    def load_annotations(self, idx):
+        # Implement this method to load and preprocess annotations
+        pass
+
 
 def get_transform(train):
-    transforms = []
-    transforms.append(transforms.ToTensor())
+    transforms_list = []
+    transforms_list.append(transforms.ToTensor())
     if train:
         # Add data augmentation transforms if training
+        # Example: transforms_list.append(transforms.RandomHorizontalFlip(0.5))
         pass
-    return transforms
+    return transforms.Compose(transforms_list)
 
+
+# Initialize your custom dataset and data loaders
 train_dataset = AerialRescue(data_dir='path_to_train_data', transforms=get_transform(train=True))
 val_dataset = AerialRescue(data_dir='path_to_val_data', transforms=get_transform(train=False))
-
 train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True, num_workers=4)
 val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False, num_workers=4)
-
+# Load the pretrained DETR model
 model = detr.DETR(num_classes=num_classes, pretrained=True)
-
-criterion = nn.CrossEntropyLoss()  # Modify this
-
+# Set up your loss function
+criterion = nn.CrossEntropyLoss()  # Modify this based on your specific loss function
+# Set up the optimizer
 optimizer = optim.AdamW(model.parameters(), lr=0.0001)
-
 num_epochs = 10
 for epoch in range(num_epochs):
     model.train()
     for images, targets in train_loader:
         optimizer.zero_grad()
-
         outputs = model(images)
-        loss = criterion(outputs['pred_logits'], targets)  # Modify this 
-
+        # Compute the loss based on your dataset's annotation format and requirements
+        # Modify this part to calculate the correct loss
+        loss = criterion(outputs['pred_logits'], targets)
         loss.backward()
         optimizer.step()
-
     model.eval()
     with torch.no_grad():
         for val_images, val_targets in val_loader:
             val_outputs = model(val_images)
             # Perform evaluation and calculate metrics here
-
-    print(f"Epoch [{epoch+1}/{num_epochs}] - Loss: {loss.item()}")
-
+    print(f"Epoch [{epoch + 1}/{num_epochs}] - Loss: {loss.item()}")
+# Save the trained model
 torch.save(model.state_dict(), 'aerial_rescue_detection_model.pth')
