f96e6405a2a88e76a6dbb4e299e443ae854052f3
mythri007 <mmeka@asu.edu>
2023-09-15 18:35:53 -0700

    Updation of .py file with initialization, and loading data

--- training_DETr_AOD.py
+++ training_DETr_AOD.py
@@ -7,63 +7,59 @@ from torchvision.datasets import CocoDetection
 from torchvision import transforms
 from torch.utils.data import DataLoader
 
-
-# Define your custom dataset
 class AerialRescue(torch.utils.data.Dataset):
     def __init__(self, data_dir, transforms=None):
-        # Initialize your dataset here, load images, and annotations
-        # You may need to implement a function to parse annotations and load images
         self.data_dir = data_dir
         self.transforms = transforms
-        # Load your data and annotations here
-        # Example: self.images = ...  # List of image file paths
-        # Example: self.annotations = ...  # List of annotation dictionaries
-
+        self.image_paths = []  # List to store image file paths
+        self.annotations = []  # List to store annotation dictionaries
+        self.load_data()
+    def load_data(self):
+        annotation_file = os.path.join(self.data_dir, 'annotations.json')
+        
+        with open(annotation_file, 'r') as json_file:
+            self.annotations = json.load(json_file)
+        
+        for annotation in self.annotations:
+            image_path = os.path.join(self.data_dir, annotation['image_file'])
+            self.image_paths.append(image_path)
     def __len__(self):
         return len(self.images)
-
     def __getitem__(self, idx):
-        # Implement this method to return an image and its annotations
+
         image_path = self.images[idx]
-        # Load and preprocess the image
+
         image = self.load_image(image_path)
         if self.transforms:
             image = self.transforms(image)
 
-        # Load and preprocess annotations (e.g., bounding boxes)
-        # Modify this part according to your dataset's annotation format
+
         annotation = self.load_annotations(idx)
         return image, annotation
-
     def load_image(self, image_path):
-        # Implement this method to load and preprocess images
-        pass
 
+        pass
     def load_annotations(self, idx):
-        # Implement this method to load and preprocess annotations
+
         pass
-
-
 def get_transform(train):
     transforms_list = []
     transforms_list.append(transforms.ToTensor())
     if train:
-        # Add data augmentation transforms if training
-        # Example: transforms_list.append(transforms.RandomHorizontalFlip(0.5))
+
+
         pass
     return transforms.Compose(transforms_list)
 
-
-# Initialize your custom dataset and data loaders
 train_dataset = AerialRescue(data_dir='path_to_train_data', transforms=get_transform(train=True))
 val_dataset = AerialRescue(data_dir='path_to_val_data', transforms=get_transform(train=False))
 train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True, num_workers=4)
 val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False, num_workers=4)
-# Load the pretrained DETR model
+
 model = detr.DETR(num_classes=num_classes, pretrained=True)
-# Set up your loss function
+
 criterion = nn.CrossEntropyLoss()  # Modify this based on your specific loss function
-# Set up the optimizer
+
 optimizer = optim.AdamW(model.parameters(), lr=0.0001)
 num_epochs = 10
 for epoch in range(num_epochs):
@@ -71,8 +67,7 @@ for epoch in range(num_epochs):
     for images, targets in train_loader:
         optimizer.zero_grad()
         outputs = model(images)
-        # Compute the loss based on your dataset's annotation format and requirements
-        # Modify this part to calculate the correct loss
+
         loss = criterion(outputs['pred_logits'], targets)
         loss.backward()
         optimizer.step()
@@ -80,7 +75,7 @@ for epoch in range(num_epochs):
     with torch.no_grad():
         for val_images, val_targets in val_loader:
             val_outputs = model(val_images)
-            # Perform evaluation and calculate metrics here
+
     print(f"Epoch [{epoch + 1}/{num_epochs}] - Loss: {loss.item()}")
-# Save the trained model
+
 torch.save(model.state_dict(), 'aerial_rescue_detection_model.pth')
